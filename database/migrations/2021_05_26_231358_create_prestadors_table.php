660b7c8ee2cf9be0615efdf938d115cd64c48ef0
Samuel Mercado <samuel.mg@protonmail.com>
2021-05-26 18:31:26 -0500

    Crea modelo y migración para Prestador
    - Crea modelo y migración para Prestador
    - Configura nombre de tabla en modelo
    - Configura modelo para no utilizar timestamps
    - Agrega llave foranea carrera_id

---
+++ database/migrations/2021_05_26_231358_create_prestadors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrestadorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prestadores', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('codigo', 20);
+            $table->string('correo');
+            $table->string('telefono', 30);
+            $table->foreignId('carrera_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestadores');
+    }
+}
