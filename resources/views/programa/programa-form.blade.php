1e2b2a6222bb073d44a829412a0f79ea53aa4dae
Samuel Mercado <samuel.mg@protonmail.com>
2021-06-14 17:28:50 -0500

    Implementa uso de partial/form-errors

--- resources/views/programa/programa-form.blade.php
+++ resources/views/programa/programa-form.blade.php
@@ -6,20 +6,7 @@
 
     <div class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800">
 
-        @if ($errors->any())
-            <div class="min-w-0 p-4 text-white bg-purple-600 rounded-lg shadow-xs">
-                <h4 class="mb-4 font-semibold">
-                Verifique los campos del formulario
-                </h4>
-                <p>
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </p>
-            </div>
-        @endif
+        @include('partials.form-errors')
 
         @if(isset($programa))
         {{-- Edición de programa --}}
